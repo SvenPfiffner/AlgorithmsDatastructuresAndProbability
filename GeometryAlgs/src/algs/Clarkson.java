f27e2b23b8bd6646df937457e9a1ab55eedbaf56
Sven Pfiffner <svenp@student.ethz.ch>
2019-05-19 11:05:02 +0200

    Ui is more or less ready

---
+++ GeometryAlgs/src/algs/Clarkson.java
@@ -0,0 +1,9 @@
+package algs;
+
+/**
+ * This class implements the clarkson min-circle algorithm
+ * @author Sven Pfiffner
+ */
+public class Clarkson {
+
+}
--- GeometryAlgs/src/application/Main.java
+++ GeometryAlgs/src/application/Main.java
@@ -1,23 +1,61 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	
+	Label instructions;
+	Button clarkson;
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
+		//Configure UI Elements
+		instructions = new Label();
+		instructions.setText("Please choose algorithm to display");
+		instructions.setAlignment(Pos.CENTER);
+		clarkson = new Button();
+		clarkson.setText("Clarkson Min Circle");
+		clarkson.setMaxWidth(Double.MAX_VALUE);
+		
+		//Configure layout
+		VBox buttonPane = new VBox(instructions,clarkson);
+		VBox.setMargin(instructions, new Insets(10));
+		VBox.setMargin(clarkson, new Insets(10));
+		buttonPane.setAlignment(Pos.TOP_CENTER);
+		
+		//Configure stage
+		Scene scene = new Scene(buttonPane, 300, 300);
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("©Sven Pfiffner");
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+	
+	public static void openDrawArea(String title, String instructions) {
+		Stage stage = new Stage();
+		
+		//Configure UI elements
+		Canvas canvas = new Canvas();
+		
+		//Configure layout
+		BorderPane bp = new BorderPane();
+		bp.setCenter(canvas);
+		
+		Scene scene = new Scene(bp, 900, 900);
+		stage.setScene(scene);
+		stage.setTitle(title);
+		stage.show();
 	}
 	
 	public static void main(String[] args) {
