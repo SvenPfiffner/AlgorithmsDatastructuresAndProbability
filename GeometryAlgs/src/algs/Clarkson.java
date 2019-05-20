7415f95a1bf7c4767df69b3b536dd5782a88f957
Sven Pfiffner <svenp@student.ethz.ch>
2019-05-20 10:45:18 +0200

    UI is ready for algorithm implementation

--- GeometryAlgs/src/algs/Clarkson.java
+++ GeometryAlgs/src/algs/Clarkson.java
@@ -1,9 +1,56 @@
 package algs;
 
+import java.util.ArrayList;
+
+import datastructures.Point;
+
 /**
  * This class implements the clarkson min-circle algorithm
  * @author Sven Pfiffner
  */
 public class Clarkson {
 
+	counterPoint[] points;
+	Point circCenter;
+	double circDiameter;
+	
+	public Clarkson(ArrayList<Point> points) {
+		this.points = new counterPoint[points.size()];
+		int i = 0;
+		for(Point p: points) {
+			this.points[i++] = new counterPoint(p.getXPos(),p.getYPos());
+		}
+	}
+	
+	/**
+	 * Find the smallest possible circle for the given points
+	 */
+	public void findMinCirc() {
+		
+	}
+	
+}
+
+class counterPoint extends Point{
+	int count = 0;
+	
+	public counterPoint(double x, double y) {
+		super(x,y);
+	}
+	
+	/**
+	 * Increment the counter on this point by one
+	 */
+	public void inc() {
+		count++;
+	}
+	
+	/**
+	 * Give the counter on this point a new value
+	 * @param v new value
+	 */
+	public void setCount(int v) {
+		count = v;
+	}
+	
 }
--- GeometryAlgs/src/application/Main.java
+++ GeometryAlgs/src/application/Main.java
@@ -1,18 +1,28 @@
 package application;
-	
+
+import java.util.ArrayList;
+
+import algs.Clarkson;
+import datastructures.Point;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	
+	static ArrayList<Point> points = new ArrayList<Point>();
 	
 	Label instructions;
 	Button clarkson;
@@ -27,6 +37,14 @@ public class Main extends Application {
 		clarkson = new Button();
 		clarkson.setText("Clarkson Min Circle");
 		clarkson.setMaxWidth(Double.MAX_VALUE);
+		
+		//Event handling
+		clarkson.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg0) {
+				openDrawArea("Clarksons Min Circle", "Test");
+			}});
 		
 		//Configure layout
 		VBox buttonPane = new VBox(instructions,clarkson);
@@ -47,15 +65,61 @@ public class Main extends Application {
 		
 		//Configure UI elements
 		Canvas canvas = new Canvas();
+		Button reset = new Button();
+		reset.setText("Reset Canvas");
 		
 		//Configure layout
 		BorderPane bp = new BorderPane();
-		bp.setCenter(canvas);
+		Pane wrapperPane = new Pane();
+		wrapperPane.getChildren().add(canvas);
+		VBox leftBox = new VBox(reset);
+		leftBox.setAlignment(Pos.CENTER);
+		bp.setCenter(wrapperPane);
+		bp.setLeft(leftBox);
+		canvas.widthProperty().bind(wrapperPane.widthProperty());
+		canvas.heightProperty().bind(wrapperPane.heightProperty());
+		VBox.setMargin(reset, new Insets(10));
+		wrapperPane.getStyleClass().add("canvas");
+		
+		//Event handling
+		reset.setOnAction(event -> {reset(canvas);});
+
+		
+		canvas.setOnMouseClicked(event -> {
+			Point p = new Point(event.getX(), event.getY());
+			points.add(p);
+			update(canvas);
+		});
 		
 		Scene scene = new Scene(bp, 900, 900);
+		scene.getStylesheets().add("/application/application.css");
 		stage.setScene(scene);
 		stage.setTitle(title);
+		stage.setResizable(false);
 		stage.show();
+	}
+	
+	public static void reset(Canvas canvas) {
+		points.clear();
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+	}
+	
+	public static void update(Canvas canvas) {
+		//Clear canvas
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		
+		//draw all points
+		for(Point p: points) {
+			p.drawOnCanvas(canvas);
+		}
+		
+		//Get min circle
+		Clarkson clarkson = new Clarkson(points);
+		clarkson.findMinCirc();
+		
+		//Draw circle
 	}
 	
 	public static void main(String[] args) {
