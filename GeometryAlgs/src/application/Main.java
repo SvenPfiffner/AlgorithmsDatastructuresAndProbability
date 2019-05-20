6a1fbc9b5c2a3034b6376532d85f0376dc1ad178
Sven Pfiffner <svenp@student.ethz.ch>
2019-05-20 14:11:20 +0200

    Skeleton for visualisation of geometry algorithms

--- GeometryAlgs/src/application/Main.java
+++ GeometryAlgs/src/application/Main.java
@@ -18,56 +18,20 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 
 public class Main extends Application {
 	
 	static ArrayList<Point> points = new ArrayList<Point>();
 	
-	Label instructions;
-	Button clarkson;
-	
 	@Override
 	public void start(Stage primaryStage) {
-		
-		//Configure UI Elements
-		instructions = new Label();
-		instructions.setText("Please choose algorithm to display");
-		instructions.setAlignment(Pos.CENTER);
-		clarkson = new Button();
-		clarkson.setText("Clarkson Min Circle");
-		clarkson.setMaxWidth(Double.MAX_VALUE);
-		
-		//Event handling
-		clarkson.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent arg0) {
-				openDrawArea("Clarksons Min Circle", "Test");
-			}});
-		
-		//Configure layout
-		VBox buttonPane = new VBox(instructions,clarkson);
-		VBox.setMargin(instructions, new Insets(10));
-		VBox.setMargin(clarkson, new Insets(10));
-		buttonPane.setAlignment(Pos.TOP_CENTER);
-		
-		//Configure stage
-		Scene scene = new Scene(buttonPane, 300, 300);
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("©Sven Pfiffner");
-		primaryStage.setResizable(false);
-		primaryStage.show();
-	}
-	
-	public static void openDrawArea(String title, String instructions) {
-		Stage stage = new Stage();
-		
 		//Configure UI elements
-		Canvas canvas = new Canvas();
-		Button reset = new Button();
+		Canvas canvas = new Canvas(); //This is the canvas where the user can draw points
+		Button reset = new Button(); //This button will reset the canvas
 		reset.setText("Reset Canvas");
-		
+				
 		//Configure layout
 		BorderPane bp = new BorderPane();
 		Pane wrapperPane = new Pane();
@@ -80,47 +44,57 @@ public class Main extends Application {
 		canvas.heightProperty().bind(wrapperPane.heightProperty());
 		VBox.setMargin(reset, new Insets(10));
 		wrapperPane.getStyleClass().add("canvas");
-		
+				
 		//Event handling
-		reset.setOnAction(event -> {reset(canvas);});
+		reset.setOnAction(event -> {
+			//TODO: Fill in functionality of reset button
+			reset(canvas);
+		});
 
-		
 		canvas.setOnMouseClicked(event -> {
+			//TODO: Fill in what happens upon mouse click on canvas
 			Point p = new Point(event.getX(), event.getY());
 			points.add(p);
 			update(canvas);
 		});
-		
+				
 		Scene scene = new Scene(bp, 900, 900);
 		scene.getStylesheets().add("/application/application.css");
-		stage.setScene(scene);
-		stage.setTitle(title);
-		stage.setResizable(false);
-		stage.show();
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("ETH Zurich 2019");
+		primaryStage.setResizable(false);
+		primaryStage.show();
 	}
 	
+	/**
+	 * Reset the canvas
+	 */
 	public static void reset(Canvas canvas) {
 		points.clear();
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 	}
 	
+	/**
+	 * Update canvas
+	 */
 	public static void update(Canvas canvas) {
 		//Clear canvas
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-		
+				
 		//draw all points
 		for(Point p: points) {
 			p.drawOnCanvas(canvas);
 		}
+				
+		//TODO: Draw desired stuff
+		/*
+		 * You can draw on the canvas using the gc. methods
+		 */
 		
-		//Get min circle
-		Clarkson clarkson = new Clarkson(points);
-		clarkson.findMinCirc();
-		
-		//Draw circle
 	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
