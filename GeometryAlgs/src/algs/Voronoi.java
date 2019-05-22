953f4197849378837a8918dae72cd6b6a7e88cb8
Sven Pfiffner <svenp@student.ethz.ch>
2019-05-22 11:22:36 +0200

    Prepared UI for voronoi

---
+++ GeometryAlgs/src/algs/Voronoi.java
@@ -0,0 +1,18 @@
+package algs;
+
+import java.util.List;
+
+import datastructures.Vector2D;
+import javafx.scene.canvas.Canvas;
+
+public class Voronoi {
+	
+	public Voronoi(List<Vector2D> points) {
+		
+	}
+	
+	public void draw(Canvas c) {
+		//TODO: Implement
+	}
+
+}
--- GeometryAlgs/src/application/Main.java
+++ GeometryAlgs/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 
 import algs.Megiddo;
+import algs.Voronoi;
 import datastructures.Vector2D;
 import javafx.application.Application;
 import javafx.geometry.Insets;
@@ -13,6 +14,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -29,6 +32,8 @@ public class Main extends Application {
 	
 	static ArrayList<Vector2D> points = new ArrayList<Vector2D>();
 	final double deleteTresh = 10;
+	final RadioButton rb1 = new RadioButton("Smallest enclosing circle");
+	final RadioButton rb2 = new RadioButton("Voronoi Diagram");
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -36,24 +41,39 @@ public class Main extends Application {
 		Canvas canvas = new Canvas(); //This is the canvas where the user can draw points
 		Button reset = new Button(); //This button will reset the canvas
 		Label instruction = new Label(); //This displays the programs instruction
+		Label radioLabel = new Label(); //Describes the radio buttons
 		reset.setText("Reset Canvas");
 		instruction.setText("Use mouse left to draw points | mouse right to delete points");
-				
+		radioLabel.setText("Choose Algorithm");
+		
+		ToggleGroup group = new ToggleGroup();
+		rb1.setToggleGroup(group);
+		rb1.setSelected(true);
+		
+		rb2.setToggleGroup(group);
+		rb2.setSelected(false);
+		
 		//Configure layout
 		BorderPane bp = new BorderPane();
 		Pane wrapperPane = new Pane();
 		wrapperPane.getChildren().add(canvas);
 		VBox leftBox = new VBox(reset);
+		VBox rightBox = new VBox(radioLabel, rb1, rb2);
 		HBox bottomBox = new HBox(instruction);
 		leftBox.setAlignment(Pos.CENTER);
+		rightBox.setAlignment(Pos.CENTER_LEFT);
 		bottomBox.setAlignment(Pos.CENTER);
 		bp.setCenter(wrapperPane);
 		bp.setLeft(leftBox);
+		bp.setRight(rightBox);
 		bp.setBottom(bottomBox);
 		canvas.widthProperty().bind(wrapperPane.widthProperty());
 		canvas.heightProperty().bind(wrapperPane.heightProperty());
 		VBox.setMargin(reset, new Insets(10));
+		VBox.setMargin(radioLabel, new Insets(10));
 		HBox.setMargin(instruction, new Insets(10));
+		HBox.setMargin(rb1, new Insets(10));
+		HBox.setMargin(rb2, new Insets(10));
 		BorderPane.setMargin(wrapperPane, new Insets(10));
 		wrapperPane.getStyleClass().add("canvas");
 				
@@ -85,10 +105,10 @@ public class Main extends Application {
 			
 		});
 				
-		Scene scene = new Scene(bp, 900, 900);
+		Scene scene = new Scene(bp, 1200, 900);
 		scene.getStylesheets().add("/application/application.css");
 		primaryStage.setScene(scene);
-		primaryStage.setTitle("ETH Zurich 2019 | Min enclosing circle by Sven Pfiffner");
+		primaryStage.setTitle("ETH Zurich 2019 | Geometry Algorithms | by Sven Pfiffner");
 		primaryStage.setResizable(false);
 		primaryStage.show();
 	}
@@ -105,7 +125,7 @@ public class Main extends Application {
 	/**
 	 * Update canvas
 	 */
-	public static void update(Canvas canvas) {
+	public void update(Canvas canvas) {
 		//Clear canvas
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -115,9 +135,18 @@ public class Main extends Application {
 			v.drawOnCanvas(canvas);
 		}
 		
-		//Find smallest circle
-		Megiddo m = new Megiddo(points);
-		m.draw(canvas);
+		//Perform choosen algorithm
+		if(rb1.isSelected()) {
+			//Find smallest circle
+			Megiddo m = new Megiddo(points);
+			m.draw(canvas);
+		} else {
+			//Perform Voronoi
+			Voronoi v = new Voronoi(points);
+			v.draw(canvas);
+		}
+		
+		
 		
 	}
 	
